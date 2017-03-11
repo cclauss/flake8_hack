647c5dd6763a18f346ba969f789bc1d855c87b9a
cclauss <cclauss@bluewin.ch>
2017-03-11 07:53:36 +0100

    Update install_python3.sh

--- install_python3.sh
+++ install_python3.sh
@@ -10,14 +10,15 @@ pip -V        # pip 6.0.8 from /usr/local/lib/python2.7/dist-packages/pip-6.0.8-
 # pip install --upgrade pip setuptools  # Error
 # pip3 -V     # Error!
 echo "start"
+ls -Fla --color
 wget https://s3.amazonaws.com/travis-python-archives/binaries/ubuntu/14.04/x86_64/python-3.6.0.tar.bz2
 tar xjf python-3.6.0.tar.bz2
 echo "finish"
 ls -Fla --color
-echo "home"
-ls -Fla --color home
-echo "opt"
-ls -Fla --color opt
+echo "home/travis"
+ls -Fla --color home/travis
+echo "opt/python"
+ls -Fla --color opt/python
 # source ~/virtualenv/python3.6.0/bin/activate
 # source ./home/travis/virtualenv/python3.6.0/bin/activate
 # whereis -b python3  # search only binaries -- python3: /usr/lib/python3
